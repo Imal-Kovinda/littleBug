ecf0653d476fd1cf0dfec13be9b1dc2ba1503abf
Imal-Kovinda <83854345+Imal-Kovinda@users.noreply.github.com>
2023-08-31 23:58:52 +0530

    AgeCal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FormsModule } from '@angular/forms'; 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -16,7 +18,8 @@ import { CalsidebarComponent } from './calsidebar/calsidebar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/container/container.component.ts
+++ src/app/container/container.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./container.component.css']
 })
 export class ContainerComponent {
+  selectedDate: string = '';
+  currentDate: string = '';
+  age: number | null = null;
 
+  onSelectedDate(newDate: string){
+    this.selectedDate = newDate;
+    
+  }
+
+  onCurrentDate(newDate: string){
+    this.currentDate = newDate;
+    
+  }
+
+  calculateAge(){
+    if(this.selectedDate && this.currentDate){
+      const birthDate = new Date(this.selectedDate);
+      const currDate = new Date(this.currentDate);
+
+      this.age = currDate.getFullYear() - birthDate.getFullYear();
+      //console.log(`Age: ${age} years`);
+    }
+  }
 }
